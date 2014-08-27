24fe45f77e535ccccb67fbdd14a51e7e08abbd44
Eyþór Magnússon <eysi09@gmail.com>
2014-08-27 14:23:51 +0200

    Fix response msg rendering bug.

--- js/is_my_flight_on_time.js
+++ js/is_my_flight_on_time.js
@@ -292,6 +292,7 @@ $(document).ready(function() {
     n_min = parseInt(n_min) > 0 ? n_min : '';
     var str_min = n_min ? 'minutes' : '';
     var status = diff > 0 ? 'late' : 'early';
+    status = diff === 0 ? 'on_time' : status;
     n_min = diff === 0 ? 'on time' : n_min;
 
     // This is what counts:
